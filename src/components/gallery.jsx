6107251f4b1ff6705a231f9a13dbef3a4cab26b7
ElicitWeb <lalitmrao@gmail.com>
2023-09-11 19:48:37 +0530

    update

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,11 +1,9 @@
 import Navbar from "./Navbar.jsx";
 import Hero from "./Hero.jsx";
-// import Gallery from "./components/Gallery";
-// import Collage from "./components/Collage";
 import Events from './events.jsx'
-// import Trial from "./components/trial";
 import Collage1 from "./Collage1.jsx";
 import "./gallery.css";
+import Footer from "./footer.jsx";
 
 const gallery = () => (
     <main className="relative">
@@ -18,8 +16,11 @@ const gallery = () => (
         <section className="Events">
             <Events/>
         </section>
-        <section className="trial">
+        <section className="Collage">
             <Collage1 />
+        </section>
+        <section className="Footer">
+            <Footer />
         </section>
     </main>
 );
