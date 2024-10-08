4b515b1311ef1dd2beccec87e3b96d6feb5a9f07
Lalit Rao <127052986+lalit-rao@users.noreply.github.com>
2024-10-09 00:40:36 +0530

    updated files

--- src/components/RTEData.js
+++ src/components/RTEData.js
@@ -1,6 +1,4 @@
-import Decode from "../assets/images/RTE23Posters/Decode.webp";
-import RoboRacing from "../assets/images/RTE23Posters/RoboRacing.webp";
-import {ElicitExpo, Futsal} from "../assets/images/index.js";
+import {Decode, ElicitExpo, Futsal, RoboRacing} from "../assets/images/index.js";
 
 export const RTEEventsList = {
     RTEEvent1: {
@@ -8,28 +6,28 @@ export const RTEEventsList = {
         RTEPoster: Futsal,
         Title: "Murder Mystery",
         Detail: "Murder Mystery is an immersive treasure hunt that promises suspense and intrigue at every turn. Participants decode clues and unravel a captivating mystery. It's an engaging and intellectually stimulating experience.",
-        Link: "http://tinyurl.com/RoadToElicit-MurderMystrey",
+        Link: "/#/upcoming-events",
     },
     RTEEvent2: {
         eventId: "2",
         RTEPoster: ElicitExpo,
         Title: "Elevate",
         Detail: "Elevate is an event centered around business strategy and negotiation, where participants present startup ideas judged by various criteria, and engage in negotiations with each other and organizers to secure necessary resources and contracts. Afterward, contracts and strategies are analyzed, and participants are assessed on factors like feasibility, planning, and resource management.",
-        Link: "http://tinyurl.com/RTE-Elevate2023",
+        Link: "/#/upcoming-events",
     },
     RTEEvent3: {
         eventId: "3",
         RTEPoster: Decode,
         Title: "Decode 3.0",
         Detail: "Decode 3.0 is an unparalleled cryptic hunt event that not only challenges the participant’s intellect but also teleports them on a mesmerizing adventure through the intricate realms of ciphers and enigmas!",
-        Link: "/Upcoming-Events",
+        Link: "/#/upcoming-events",
     },
     RTEEvent4: {
         eventId: "4",
         RTEPoster: RoboRacing,
         Title: "Futsal",
         Detail: "A 3v3 football extravaganza, where excuses are left behind, and the focus is on scoring goals and having a blast. RTE's Futsal is the ultimate gathering for football enthusiasts.",
-        Link: "http://tinyurl.com/Road-to-Elicit2023-Futsal",
+        Link: "/#/upcoming-events",
     },
     RTEEvent5: {
         eventId: "",
