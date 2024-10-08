21fefc7478899cca8eee2f1c6e189104ac48faf8
Lalit Rao <127052986+lalit-rao@users.noreply.github.com>
2024-10-08 12:18:44 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import LandingPage from "./components/landingPage.jsx";
 import "./App.css";
 import UpcomingEventsMainPage from "./components/upcomingEventsMainPage.jsx";
-import Teams from "./components/teamsMainPage.jsx";
+// import Teams from "./components/teamsMainPage.jsx";
 import GalleryPage from "./components/gallery.jsx";
 
 
@@ -10,14 +10,14 @@ const App = () => {
     return (
         <>
             <div className="RootDiv">
-                <BrowserRouter basename="/">
+                <HashRouter>
                     <Routes>
                         <Route path="/" element={<LandingPage/>}/>
                         <Route path="Gallery" element={<GalleryPage/>}/>
                         <Route path="Upcoming-Events" element={<UpcomingEventsMainPage/>}/>
-                        <Route path="Team" element={<Teams/>}/>
+                        {/*<Route path="Team" element={<Teams/>}/>*/}
                     </Routes>
-                </BrowserRouter>
+                </HashRouter>
             </div>
         </>
     );
